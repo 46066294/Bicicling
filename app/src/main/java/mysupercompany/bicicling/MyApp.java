45a505401a3312dad4ddc84e42c7da3e51f097f4
46066294 <46066294p@iespoblenou.org>
2017-02-08 15:31:22 +0100

    Initial commit

---
+++ app/src/main/java/mysupercompany/bicicling/MyApp.java
@@ -0,0 +1,33 @@
+package mysupercompany.bicicling;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+
+/**
+ * Created by Mat on 08/02/2017.
+ */
+
+public class MyApp extends Application {
+
+    private Firebase ref;
+
+    public Firebase getRef() {
+        return ref;
+    }
+
+    public void setRef(Firebase ref) {
+        this.ref = ref;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        super.onCreate();
+        Firebase.setAndroidContext(this);
+
+        ref = new Firebase("https://todos-a80c7.firebaseio.com/");
+    }
+}
